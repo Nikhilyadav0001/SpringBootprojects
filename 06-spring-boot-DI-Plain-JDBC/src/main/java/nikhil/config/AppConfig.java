a05395d09d6e01e4f761e775cfe3c4c499b8d7fd
nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-03-22 23:18:01 +0530

    DI project using spring boot and using diffent dataSource

--- 06-spring-boot-DI-Plain-JDBC/src/main/java/nikhil/config/AppConfig.java
+++ 06-spring-boot-DI-Plain-JDBC/src/main/java/nikhil/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-//@Import(value=PersistenceConfig.class)
+@Import(value=PersistenceConfig.class)
 public class AppConfig {
-
+//this is not necessary so dont create this class
 }
