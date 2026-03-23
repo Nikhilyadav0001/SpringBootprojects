3779c1f31b68315ebd41c5b27f04581728d39e31
nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-03-23 19:46:08 +0530

    profiles in spring boot

---
+++ 08-spring-boot-profile-app/src/main/java/nikhil/dao/EmployeeDaoImpl.java
@@ -0,0 +1,69 @@
+package nikhil.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import nikhil.beans.EmployeeBO;
+
+@Repository("dao")
+public class EmployeeDaoImpl implements IEmployeeDao {
+
+	private static final String GET_EMP_DESG = "SELECT * FROM employee Where job IN";
+	@Autowired
+	private DataSource dataSource;
+	
+	@Override
+	public List<EmployeeBO> fetchEmpByDesg(String cond) throws Exception {
+		
+		List<EmployeeBO> listBo =null;
+		EmployeeBO bo =null;
+		
+		try(Connection connection =dataSource.getConnection();
+				//USE Connection and perform db operation using plain jdbc
+				Statement stmt =connection.createStatement();
+				//use result set to pull tthe records
+				ResultSet resultSet =stmt.executeQuery(GET_EMP_DESG+cond+ "ORDER BY job")
+						){
+			
+			System.out.println("datasource type is::"+dataSource.getClass().getName());
+			
+			
+			listBo =new ArrayList<>();
+			//process resultset
+			while (resultSet.next()) {
+				//copy record form rs to bo
+				bo = new EmployeeBO();
+				bo.setEmpNo(resultSet.getInt(1));
+				bo.setEmpName(resultSet.getString(2));
+				bo.setJob(resultSet.getString(3));
+				bo.setSalary(resultSet.getFloat(4));
+				bo.setDeptNo(resultSet.getInt(5));
+				bo.setMgrNo(resultSet.getInt(6));
+				
+				listBo.add(bo);
+			}
+			
+			
+		}catch (SQLException se) {
+			se.printStackTrace();
+			throw se;
+		}catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		
+		
+		
+		return listBo;
+	}
+
+}
