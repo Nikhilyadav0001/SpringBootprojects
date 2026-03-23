dd892a303435f29b35b578420f5fc36806ffe91f
nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-03-23 23:01:21 +0530

    commandline runner

---
+++ 10-spring-boot-Runner-app/src/main/java/nikhil/runner/StudentRunner.java
@@ -0,0 +1,50 @@
+package nikhil.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import nikhil.beans.Employee;
+import nikhil.beans.Student;
+
+@Component
+@Order(5)
+public class StudentRunner implements CommandLineRunner, Ordered {
+
+	@Autowired
+	private Employee employee;
+
+	@Autowired
+	private Student student;
+
+	static {
+		System.out.println("StudentRunner.class file is loading...");
+	}
+
+	public StudentRunner() {
+		System.out.println("StudentRunner Object is created...");
+	}
+
+	// Called by the container automatically
+	// After creation of all object of singleton scope
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("\nStudentRunner.run()");
+		System.out.println(employee);
+		System.out.println(student);
+
+		System.out.println("Arguments passed are :: ");
+		for (String cmd : args) {
+			System.out.println(cmd);
+		}
+	}
+
+	@Override
+	public int getOrder() {
+		System.out.println("StudentRunner.getOrder()");
+		return 5;
+	}
+
+}
